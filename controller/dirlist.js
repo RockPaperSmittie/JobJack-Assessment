e89a2934d9e181c0f504d43babdf931a7268b1ad
Ruan <rockpapersmittie@gmail.com>
2019-06-04 11:46:04 +0200

    Added CORS headers to index.js

--- controller/dirlist.js
+++ controller/dirlist.js
@@ -1,20 +1,32 @@
 const readdir = require('readdir-enhanced');
 const os = require('os');
-
 const through2 = require('through2');
 
+//Returns directory contents in JSON format
 exports.getDir = (req, res, next) => {
+    //Stores absolute directory to prepend to inputDir
     homeDir = os.homedir()
-    let datainfo = []
+    //Directory input on client
     let inputDir = "/desktop/JobJack-Assessment"
-    
+    //Empty array that will hold directory contents
+    let dataInfo = []
+
+    //Stream data from given directory and piped to dataInfo array
     let stream = readdir.stream.stat(homeDir + inputDir)
     .pipe(through2.obj((data, enc, next) => {
-      datainfo.push({size: data.size, path: homeDir + inputDir, name: data.path});
+        let fileType = data.path.split('.').pop();
+        dataInfo.push({size: data.size, 
+            path: homeDir + inputDir, 
+            name: data.path,
+            filetype: fileType});
       next();
     }));
+    //allows for data to be streamed before response is sent
+
     setTimeout(() => {
-        res.status(200).json(datainfo)
+        res.status(200).json(dataInfo)
+        console.log(dataInfo.length)
     }, 1000);
-}
+
+};
 
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const dirListRoutes = require('./routes/dirlist.js');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Controll-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/', dirListRoutes);
 
